523b5fecc0a3ad77cb571b18d6ecc891ea2fbd03
Christopher Sharp <chris3024@live.com>
2023-04-22 16:57:10 -0500

    Test

---
+++ CMakeFiles/ABCU_Program.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "ABCU_Program"
+  "ABCU_Program.pdb"
+  "CMakeFiles/ABCU_Program.dir/main.cpp.o"
+  "CMakeFiles/ABCU_Program.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ABCU_Program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/chris3024/CS:300/ProjectTwo
+# Build directory: /home/chris3024/CS:300/ProjectTwo/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
